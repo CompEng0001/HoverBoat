83fcf791b513144f0ea87d88c8bc886b65d1bbf4
Rich Blair <rb5356h@greenwich.ac.uk>
2019-02-16 16:36:14 +0000

    Update HoverBoat.ino
    Added Apache 2.0 License

--- HoverBoat/HoverBoat.ino
+++ HoverBoat/HoverBoat.ino
@@ -1,9 +1,23 @@
-/* Created by : Richard Blair
-   Creation Date: 25/06/2018
-   Latest Modified Date: 16/02/2019
-   Version Number: 1.5
-   Gitrepository - https://github.com/CompEng0001/HoverBoat
+/*------------------------------------------------------------------------
+  Copyright [2019] [Richard Blair]
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  ------------------------------------------------------------------------
+  Creation Date: 25/06/2018
+  Latest Modified Date: 16/02/2019
+  Version Number: 1.5
+  Licence: Apache 2.0
+  Gitrepository - https://github.com/CompEng0001/HoverBoat
+  -----------------------------------------------------------------------
    Notes - This sketch is for use with Year 0's project 3. The sketch has been written to take instructions from the serial specifically when used with HC-05.
 */
 #include <Servo.h> //  DON'T NOT CHANGE THIS LIBRARY IS ESSIENTIAL
